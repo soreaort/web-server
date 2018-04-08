f0acd097aff0009a0cb455033bd0c3ca217aa959
Saul Orea <soreaort@gmail.com>
2018-04-08 09:05:10 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# web-server
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: web-server
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
